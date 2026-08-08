f2709e303fd8761d00180cd8a74a3486366d4ebb
tusharx0809 <tusharbhatia0809@outlook.com>
2026-08-08 11:26:47 +0530

    added bubble sort and selction sort

---
+++ Arrays/BubbleSort.py
@@ -0,0 +1,17 @@
+"""Bubble sort"""
+
+def bubbleSort(arr: list[int]) -> None:
+    size = len(arr)
+    for i in range(0, size):
+        swapped = False
+        for j in range(0,size-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+arr = [5,1,6,2,3,8,7,9,0]
+bubbleSort(arr)
+
+print(arr)
---
+++ Arrays/SelectionSort.py
@@ -0,0 +1,15 @@
+"""Selection Sort"""
+
+def selectionSort(arr: list[int]) -> None:
+    size = len(arr)
+    for i in range(0,size):
+        min_idx = i
+        for j in range(i+1, size):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[min_idx], arr[i] = arr[i], arr[min_idx]
+
+arr = [5,1,6,2,3,8,7,9,0]
+selectionSort(arr)
+print(arr)
