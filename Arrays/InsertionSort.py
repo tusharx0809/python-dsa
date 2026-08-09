156a0f2b5fc3b2a60a5b1ed1eaeb0f812832eb38
tusharx0809 <tusharbhatia0809@outlook.com>
2026-08-09 13:02:52 +0530

    added a condition in insertion sort

--- Arrays/InsertionSort.py
+++ Arrays/InsertionSort.py
@@ -9,7 +9,8 @@ def insertionSort(arr: list[int]) -> None:
         while j >= 0 and arr[j] > key:
             arr[j+1] = arr[j]
             j = j-1
-        arr[j+1] = key
+        if j != i-1:
+            arr[j+1] = key
 
 arr = [5,1,6,2,3,8,7,9,0]
 insertionSort(arr)
