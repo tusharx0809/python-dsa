dea3abf6eff5e7406a3b36bc10fbd6a41d291ee5
tusharx0809 <tusharbhatia0809@outlook.com>
2026-08-15 17:33:13 +0530

    added print statements for understanding

--- Arrays/MergeSort.py
+++ Arrays/MergeSort.py
@@ -1,12 +1,15 @@
 """MERGE SORT"""
 
 def merge(arr: list[int], left:int, mid:int, right: int) -> None:
+    #print(f"arr: {arr}, left: {left}, mid: {mid}, right: {right}")
     n1: int = mid-left+1
     n2: int = right-mid
-
+    #print("n1:", n1)
+    #print("n2:", n2)
     l: list[int] = [arr[left+i] for i in range(0,n1)]
     r: list[int] = [arr[mid+1+i] for i in range(0,n2)]
-
+    #print("l",l)
+    #print("r",r)
     i,j,k = 0,0,left
 
     while i < n1 and j < n2:
@@ -28,7 +31,6 @@ def merge(arr: list[int], left:int, mid:int, right: int) -> None:
         k += 1
 
 def mergeSort(arr: list[int], left: int, right: int) -> None:
-
     if left >= right:
         return
     mid: int = left + (right - left) // 2
---
+++ Arrays/QuickSort.py
@@ -0,0 +1,11 @@
+"""QUICK SORT"""
+
+
+
+def main():
+    arr: list[int] = [5,1,6,2,3,8,7,9,0]
+        #quickSort(arr,0,len(arr)-1)
+    print(arr)
+    
+    if __name__ == "__main__":
+        main()
